9cb2c8bc7cc4acfa8e234d412b2180961616c6bc
Мариша <mkot6507@gmail.com>
2023-08-03 22:26:08 +1000

    Домашка лекция №7

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+
+    # Товар
+    # строковое поле небольших размеров
+    # "заголовок" - verbose_name - название поле извне
+    title = models.CharField('заголовок', max_length =128)
+
+    # Описание товара
+    # Большое текстовое поле, для больших текстов
+    description = models.TextField('описание')
+
+    # Цена
+    # Специальный тип данных с фиксированной точкой 
+    price = models.DecimalField('цена', max_digits=10, decimal_places=2 )
+    
+    # Дата публикации
+    # Поле записывается при создании объявления
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Дата изменения/обновления
+    # Поле записывается при каждом обновлении
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Уместен ли торг
+    # Логический тип, два значения - правда или ложь
+    auction = models.BooleanField('торг',  help_text = 'Отметьте, уместен ли торг')
+
+    class Meta: 
+        db_table = 'advertisements'
+
+    # Актуальность объявления
+    # Количество товара
+    # Имя продавца + контакты
+    # Возможен ли обмен
+    # Адрес продажи/осмотре
+    # Б/у товар или нет
+    # Возможно ли взять в долг/рассрочку
